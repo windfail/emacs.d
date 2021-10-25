7992c64d7b313c7d4c5b88e48539132bb5c4ccff
GriffOn <yily_101@163.com>
2021-10-25 10:59:27 +0800

    ami file parse change to elisp

--- lisp/init-griffon.el
+++ lisp/init-griffon.el
@@ -74,27 +74,62 @@
     (setq pkgname (nth 1 bmc-dir))
     (compile (format "bash -c \"cd %s; sudo project.py -b %s.spx --rebuild\""  prjdir pkgname ))))
 
+(defun ami-parse-makefile (builddir makefile)
+  "This function parse  MAKEFILE, find includ dirs in CFLAGS -I lines, replace SPXINC/TARGDIR with correct dir using  BUILDDIR ."
+  (let ((dirlist (list (list "SPXINC"  (format "%s/include" builddir))
+                       (list "TARGETDIR"  (format "%s/target" builddir))
+                       (list nil  "")))
+        (inclist nil))
+    (with-temp-buffer
+      (insert-file-contents makefile)
+      (rx-let ((var_rx (: "$" (any "{(")
+                          (group-n 1 (+ (not (any "${()}"))))
+                          (any ")}")))
+               (cflag_rx (: line-start (* (any " \t")) "CFLAGS" (+ (any " \t+=")) "-I"
+                            (opt var_rx)
+                            (group-n 2 (+ (not (any " \t#\n${}")))))))
+        (while (re-search-forward (rx cflag_rx) nil t )
+          (setq idir (format "%s%s"
+                             (car (cdr (assoc (match-string 1) dirlist)))
+                             (match-string 2)))
+          (setq inclist (cons idir inclist)))))
+    (nreverse inclist)))
+(defun ami-parse-prjdef (prjheader)
+  "This function parse PRJHEADER, change the defines into list of gcc definitions for providing to flycheck."
+  (let ((ami-defs nil))
+    (with-temp-buffer
+      (insert-file-contents prjheader)
+      (rx-let ((def_rx (: "define" (+ (any " \t"))
+                          (group (+ (not (any " \t"))))
+                          (+ (any " \t"))
+                          (group (+ nonl)))))
+        (while (re-search-forward (rx def_rx) nil t)
+          (setq ami-defs
+                (cons (format "%s=%s"
+                              (match-string 1)
+                              (match-string 2))
+                      ami-defs)))))
+    (nreverse ami-defs)))
 (defun ami-bmc-include ()
   "Add bmc include dir."
-  (setq bmc-dir (ami-mds-dir-parse))
-  (when bmc-dir
-    (setq prjdir (car bmc-dir))
-    (setq pkgname (nth 1 bmc-dir))
-    (setq makefile (format "%s/workspace/source/%s/data/Makefile" prjdir pkgname))
-    (setq include-path
-          (cons
-           (format "%s/workspace/tools/arm-linux/arm-none-linux-gnueabi/arm-none-linux-gnueabi/sysroot/usr/include" prjdir)
-           (process-lines
-            "sed" "-n"
-            "-e" (format "s|\\$[{(]SPXINC[})]|%s/workspace/Build/include|" prjdir) ;;replace $SPXINC
-            "-e" (format "s|\\$[{(]TARGETDIR[})]|%s/workspace/Build/target|" prjdir) ;;replace $TARGETDIR
-            "-e" "/^[ \t]*CFLAGS[ \t]*+=[ \t]*-I/s/.*CFLAGS[ \t]*+=[ \t]*-I\\([^# \t]*\\).*$/\\1/p" 
-            makefile)))
-    (setq ami-defs (list "UN_USED(x)=(void)(x)"))
-    (setq flycheck-disabled-checkers '(c/c++-clang)) ;; disable clang for ami code
-    (setq flycheck-gcc-include-path include-path)
-    (setq flycheck-cppcheck-include-path include-path)
-    (setq flycheck-gcc-definitions ami-defs)))
+  (let ((bmc-dir (ami-mds-dir-parse)))
+    (when bmc-dir
+      (let* ((workdir (format "%s/workspace" (car bmc-dir)))
+             (pkgname (nth 1 bmc-dir))
+             (makefile (format "%s/source/%s/data/Makefile" workdir pkgname))
+             (builddir (format "%s/Build" workdir))
+             (prjheader (format "%s/include/projdef.h" builddir))
+             (sysinc (format "%s/tools/arm-linux/arm-none-linux-gnueabi/arm-none-linux-gnueabi/sysroot/usr/include" workdir))
+             (include-path (cons sysinc
+                                 (ami-parse-makefile builddir makefile)))
+             (ami-defs (cons "UN_USED(x)=(void)(x)"
+                             (ami-parse-prjdef prjheader)) )
+             )
+        (setq flycheck-disabled-checkers '(c/c++-clang)) ;; disable clang for ami code
+        (setq flycheck-gcc-include-path include-path)
+        (setq flycheck-cppcheck-include-path include-path)
+        (setq flycheck-gcc-definitions ami-defs))
+      )))
 
 (add-hook 'flycheck-before-syntax-check-hook 'ami-bmc-include)
 (provide 'init-griffon)
